19e3fd7075c387fa8226765c21ade608d3040542
dima <dmitriy.dubchenko@gmail.com>
2019-04-25 16:05:33 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import MyComponent from "./components/app.component";
+
+ReactDOM.render(<MyComponent />, document.querySelector("#root"));
